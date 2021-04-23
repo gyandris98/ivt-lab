f0abd4861f41bea61017db7bc0c3108187b4b3fe
Győrffy András <gy.andris98@gmail.com>
2021-04-23 14:35:38 +0200

    Refactor illegal argument exception to throw a more specific error

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -32,7 +32,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("Number of torpedos is less than one or not enough torpedos left");
     }
 
     boolean success = false;
